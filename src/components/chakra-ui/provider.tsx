5e1cfb698dd60caf0e61b7bf38a295e71c6ebcd2
Dominik Kralj <kraljdominik97@gmail.com>
2025-10-27 18:43:37 +0100

    Fix hydration warning

--- src/components/chakra-ui/provider.tsx
+++ src/components/chakra-ui/provider.tsx
@@ -1,8 +1,18 @@
 'use client';
 
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 
 export function Provider({ children }: { children: ReactNode }) {
+	const [mounted, setMounted] = useState(false);
+
+	useEffect(() => {
+		setMounted(true);
+	}, []);
+
+	if (!mounted) {
+		return null;
+	}
+
 	return <ChakraProvider value={defaultSystem}>{children}</ChakraProvider>;
 }
